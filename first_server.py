1becd26de0ca99a988cd9efa991faef4dd680fd8
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-08-08 23:41:01 +0530

    intial : Basic get endpoints

--- first_server.py
+++ first_server.py
@@ -2,8 +2,39 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/start')
+@app.get('/')
 async def root(): 
     return{
-        "message" : "Hello world"
+        "message" : "landing page endpoint"
     }
+
+@app.get('/home')
+async def home():
+    return {
+        "String": "home endpoint"
+    }
+
+#endpoint parameteres
+
+@app.get("/path_params/{name}")
+async def endpoint_params(name : str):
+    return {
+        "enpoint params : name" : name
+    }
+
+#path operations 
+@app.get('/users/me') #this should be declared earlier 
+async def usersMe():
+    return {
+        "endpoint" : "user/me : current user"
+    }
+
+@app.get("/users/{user_id}")
+async def read_user(user_id : str):
+    return {
+        "user" : user_id
+    }
+
+@app.get('/twovalues')
+async def val():
+    return ["Ooga" , "booga"]
