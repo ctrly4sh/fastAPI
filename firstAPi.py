9c0e1b6e57c9488298d087ac285fb4655dd069dd
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-08-18 18:54:46 +0530

    basics ????

---
+++ firstAPi.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def home():
+    return{
+        "message" : "Server started "
+    }
+    
+@app.get("/items/{item_id}")
+async def items(item_id : int):
+    return("Item id is :" , item_id)
